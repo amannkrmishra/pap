7ed7e63e81f72662eccd1c158a1194d392d3f21a
Aman <130031494+amannkrmishra@users.noreply.github.com>
2025-10-03 00:04:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3620,7 +3620,7 @@ client.on('ready', async () => {
         cron.schedule('0 12 * * *', runDailySubscriptionNotifier, { timezone: "Asia/Kolkata" });
 
         // Daily subscriber report and CSV download
-        cron.schedule('59 23 * * *', scheduledTask, { timezone: "Asia/Kolkata" });
+        cron.schedule('8 24 * * *', scheduledTask, { timezone: "Asia/Kolkata" });
 
         // Finally, start the main proactive refresh timer for subsequent runs
         setInterval(forceRefreshSession, AUTH_LIFETIME);
@@ -3659,3 +3659,4 @@ client.on('message', (message) => {
 
 
 client.initialize();
+
